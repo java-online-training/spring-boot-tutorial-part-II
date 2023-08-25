d62a850643dea55094c43a3219211e0a0cd19edd
Ernesto Elias-Nieland <superernie77@gmail.com>
2023-08-25 12:13:18 +0200

    URIComponentsBuilder example

---
+++ src/test/java/com/jot/SpringBootTutorialPartIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialPartIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
