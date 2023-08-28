c4b01e33c5661a3e6bf0abd60e3c319f65fd4ca8
Ernesto Elias-Nieland <superernie77@gmail.com>
2023-08-28 11:17:41 +0200

    unused imports removed

--- src/main/java/com/jot/rest/BookController.java
+++ src/main/java/com/jot/rest/BookController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
